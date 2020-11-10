efaf0890ee22e14cf2ee115e15876279259ff3a3
wecassidy <wec.notif.github@gmail.com>
2020-11-10 03:35:47 -0500

    Visualize training/val datasets

--- train.py
+++ train.py
@@ -7,6 +7,7 @@ Train license plate NNs
 import os
 
 import keras
+import matplotlib.pyplot as plt
 
 import util
 
@@ -24,7 +25,6 @@ def load_id_dataset(subset):
     return keras.preprocessing.image_dataset_from_directory(
         os.path.join(IMAGE_DIR, "ids/train"),
         labels="inferred",
-        label_mode="categorical",
         image_size=util.image_shape[:2],
         validation_split=0.2,
         subset=subset,
@@ -32,8 +32,24 @@ def load_id_dataset(subset):
     )
 
 
+def visualize_dataset(dataset):
+    """Preview a few images and labels from a set to make sure everything is in order."""
+    plt.figure()
+    for images, labels in dataset.take(1):
+        for i in range(9):
+            plt.subplot(3, 3, i + 1)
+            plt.imshow(images[i].numpy().astype("uint8"))
+            plt.title(dataset.class_names[labels[i]])
+            plt.axis("off")
+    plt.tight_layout()
+    plt.show()
+
+
 if __name__ == "__main__":
     ensure_output_dirs()
 
     ids_train = load_id_dataset("training")
     ids_validation = load_id_dataset("validation")
+
+    visualize_dataset(ids_train)
+    visualize_dataset(ids_validation)
