f90d74462bbbff350ef75d752261f7fb2d27d5c2
wecassidy <wec.notif.github@gmail.com>
2020-11-15 16:11:59 -0500

    Shrink model for no particularly good reason

--- models.py
+++ models.py
@@ -5,21 +5,16 @@ Neural networks for plate identification (which plate is in frame?) and plate re
 (what is the number on the license plate in frame?).
 """
 
-from __future__ import division
-
 from keras import models, layers, optimizers
 
 
 def id_model(input_shape):
     """Set up the plate ID NN"""
     model = models.Sequential()
-    model.add(layers.Conv2D(5, 5, activation="relu"))
-    model.add(layers.MaxPooling2D())
-    model.add(layers.Conv2D(8, 3, activation="relu"))
+    model.add(layers.Conv2D(5, 5, activation="relu", input_shape=input_shape))
     model.add(layers.MaxPooling2D())
     model.add(layers.Flatten())
     model.add(layers.Dropout(0.5))
-    model.add(layers.Dense(81, activation="relu"))
     model.add(layers.Dense(27, activation="relu"))
     model.add(layers.Dense(9, activation="softmax"))
     model.compile(
